42689f037760e200d19aa4f7a512f751eb4dfea5
Ian <roaianchristopher@gmail.com>
2026-08-17 20:28:27 +0800

    Disable Metal for iOS FFmpeg build

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -638,6 +638,7 @@ echo "============================================================"
     --enable-static \
     --disable-shared \
     --enable-pic \
+    --disable-metal \
     \
     --disable-avdevice \
     --enable-avformat \
